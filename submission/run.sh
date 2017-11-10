4d3fab74646ce5625fcf5bfc336d5af5159053af
Arnold Koroa <arnold.koroa@gmail.com>
2017-11-10 13:57:32 +0800

    fix glove download commands

--- submission/run.sh
+++ submission/run.sh
@@ -1,13 +1,13 @@
 echo "Download GloVe Word Vectors"
-#wget http://nlp.stanford.edu/data/glove.6B.zip data/glove.6B.zip
-#unzip data/glove.6B.zip
-#rm data/glove.6B.zip 
+wget http://nlp.stanford.edu/data/glove.6B.zip -O data/glove.6B.zip
+unzip data/glove.6B.zip -d data/
+rm data/glove.6B.zip 
 
 echo "Install environment"
 pip install -r requirements.txt
 
 echo "Download nltk model for word tokenizer"
-#python -m nltk.downloader all
+python -m nltk.downloader all
 
 echo "Train Model"
 python train.py
